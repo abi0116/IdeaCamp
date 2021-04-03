8cf78e9625e85948abe0b64d019e839d53364596
abi0116 <kosukeabiru.www@gmail.com>
2021-04-03 15:08:33 +0000

    4/3 最終

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permit_parameters, if: :devise_controller?
-  
+
   protected
-  
+
   def configure_permit_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys: [:last_name,:first_name,:postal_code,:address,:telephone_number])
     devise_parameter_sanitizer.permit(:sign_in,keys: [:last_name,:first_name])
